b0facb421c4cbd686503a1d7bdc0e5d75b571fc6
Var1un6 <131979404+Var1un6@users.noreply.github.com>
2023-04-28 16:03:01 +0530

    Add files via upload

---
+++ prg7.sh
@@ -0,0 +1,10 @@
+$./test.sh Zara Ali
+File Name : ./test.sh
+First Parameter : Zara
+Second Parameter : Ali
+Quoted Values: Zara Ali
+Quoted Values: Zara Ali
+Total Number of Parameters : 2
+$echo $?
+0
+$
